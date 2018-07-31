f3f64edb25f023e01654071018b612f283d94ac3
Vadym Parakonnyi <v.parakonnyi@gmail.com>
2018-07-31 21:15:36 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,6 @@
 apt update ; \
 apt install -y curl ; \
 hash -r ; \
-sh -c "$(curl -sSL https://github.com/voznik/termux-config/raw/master/scripts/setup.sh)"
+sh -c "$(curl -fsSL https://github.com/voznik/termux-config/raw/master/scripts/setup.sh)" ;\
+### customize zsh
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
